863a064032c4fe1d6c66e3c68ffc88add3b49ae0
Denys Lins <denyslins@gmail.com>
2019-06-30 00:58:33 -0300

    Changing to Vue project

---
+++ client/src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Denys Lins",
+  data() {
+    return {
+      msg: "Denys Lins"
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
