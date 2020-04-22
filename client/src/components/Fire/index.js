fad73e2670ffc82af66ee08b1a15744997641a0b
Denys André dos Santos Lins <denyslins@gmail.com>
2020-04-22 01:05:56 -0300

    Render fire data structure on screen

--- client/src/components/Fire/index.js
+++ client/src/components/Fire/index.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css'
 
 function Fire () {
+  const [firePixelArray, setFirePixelArray] = useState([])
+  const [fireWidth, setfireWidth] = useState(4)
+  const [fireHeight, setfireHeight] = useState(3)
+  const createFireDateStructure = () => {
+    const numberOfPixels = fireHeight * fireWidth
+    setFirePixelArray([...firePixelArray, new Array(numberOfPixels).fill(0)])
+  }
+  const calculateFirePropagation = () => {}
+  const renderFire = () => {}
+
+  useEffect(() => {
+    createFireDateStructure()
+  }, [])
+
   return (
     <div className="Fire">
-      <h2>Denys</h2>
+      <table cellPadding={0} cellSpacing={0}>
+        {new Array(fireHeight).fill(0).map((row, i) => {
+          return (
+            <tr key={i}>
+              {new Array(fireWidth).fill(0).map((column, j) => {
+                const pixelIndex = j + fireWidth * i
+                return <td key={j}>{pixelIndex}</td>
+              })}
+            </tr>
+          )
+        })}
+      </table>
     </div>
   )
 }
