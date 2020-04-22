f008a91f47ef209c9913bc5e516e5f2b5a47cd52
Denys André dos Santos Lins <denyslins@gmail.com>
2020-04-22 00:01:00 -0300

    Add Fire component

--- client/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import App from './App'
-
-test('renders H1', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/Denys/i)
-  expect(linkElement).toBeInTheDocument()
-})
