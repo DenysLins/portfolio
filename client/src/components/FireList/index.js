d4f06cc3563905d56c8e025f4ac504574319747c
Denys Lins <denys.lins@hp.com>
2020-04-23 00:24:03 -0300

    Adjust FireLis to fill screen

--- client/src/components/FireList/index.js
+++ client/src/components/FireList/index.js
@@ -5,50 +5,32 @@ import Fire from '../Fire/index'
 
 const fireColorsPalette = [
   { r: 7, g: 7, b: 7 },
-  { r: 31, g: 7, b: 7 },
   { r: 47, g: 15, b: 7 },
-  { r: 71, g: 15, b: 7 },
   { r: 87, g: 23, b: 7 },
-  { r: 103, g: 31, b: 7 },
   { r: 119, g: 31, b: 7 },
-  { r: 143, g: 39, b: 7 },
   { r: 159, g: 47, b: 7 },
-  { r: 175, g: 63, b: 7 },
   { r: 191, g: 71, b: 7 },
-  { r: 199, g: 71, b: 7 },
   { r: 223, g: 79, b: 7 },
   { r: 223, g: 87, b: 7 },
-  { r: 223, g: 87, b: 7 },
   { r: 215, g: 95, b: 7 },
-  { r: 215, g: 95, b: 7 },
-  { r: 215, g: 103, b: 15 },
   { r: 207, g: 111, b: 15 },
-  { r: 207, g: 119, b: 15 },
   { r: 207, g: 127, b: 15 },
-  { r: 207, g: 135, b: 23 },
   { r: 199, g: 135, b: 23 },
-  { r: 199, g: 143, b: 23 },
   { r: 199, g: 151, b: 31 },
   { r: 191, g: 159, b: 31 },
-  { r: 191, g: 159, b: 31 },
   { r: 191, g: 167, b: 39 },
-  { r: 191, g: 167, b: 39 },
-  { r: 191, g: 175, b: 47 },
   { r: 183, g: 175, b: 47 },
-  { r: 183, g: 183, b: 47 },
   { r: 183, g: 183, b: 55 },
-  { r: 207, g: 207, b: 111 },
   { r: 223, g: 223, b: 159 },
-  { r: 239, g: 239, b: 199 },
   { r: 255, g: 255, b: 255 }
 ]
 
 const FireList = (props) => {
   const [firePixelArray, setFirePixelArray] = useState([])
-  const [fireWidth, setfireWidth] = useState(50)
-  const [fireHeight, setfireHeight] = useState(36)
+  const [fireWidth, setfireWidth] = useState(40)
+  const [fireHeight, setfireHeight] = useState(20)
   const [decay, setDecay] = useState(3)
-  const [time, setTime] = useState(1)
+  const [time, setTime] = useState(200)
   const [debug, setDebug] = useState(false)
 
   const calculateFirePropagation = () => {
@@ -63,7 +45,7 @@ const FireList = (props) => {
           const calculatedFireIntensity = belowPixelFireIntensity - randomDecay
           const newFireIntensity =
             calculatedFireIntensity > 0 ? calculatedFireIntensity : 0
-          newArr[currentPixelIndex] = newFireIntensity
+          newArr[currentPixelIndex - randomDecay] = newFireIntensity
         }
       }
     }
@@ -71,11 +53,12 @@ const FireList = (props) => {
   }
 
   const createFireDataStructure = () => {
+    console.log(window.innerWidth, window.innerHeight)
     const numberOfPixels = fireHeight * fireWidth
     const newArr = new Array(numberOfPixels).fill(0)
     for (let column = 0; column < fireWidth; column++) {
       const pixelIndex = numberOfPixels - fireWidth + column
-      newArr[pixelIndex] = 36
+      newArr[pixelIndex] = 18
     }
     setFirePixelArray(newArr)
   }
@@ -106,9 +89,11 @@ const FireList = (props) => {
                   <td
                     key={`${i}${j}`}
                     style={{
+                      width: window.innerWidth / fireWidth,
+                      height: window.innerHeight / fireHeight,
                       backgroundColor: color
                         ? !debug
-                          ? `rgb(${color.r},${color.g},${color.b})`
+                          ? `rgba(${color.r},${color.g},${color.b}, 0.7)`
                           : 'rgb(255,255,255)'
                         : !debug
                           ? 'rgb(0,0,0)'
