7190d2f27eb2356bfd758c74f35715f5f9a76569
Denys André dos Santos Lins <denyslins@gmail.com>
2020-04-23 00:24:03 -0300

    Adjust FireLis to fill screen

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -6,7 +6,6 @@ const App = () => {
   const [started, setStarted] = useState(false)
   return (
     <div className="App">
-      <h1>Denys</h1>
       <FireList started={started} setStarted={setStarted} />
     </div>
   )
