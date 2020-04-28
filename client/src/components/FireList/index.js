c3f996cc7644fdf869de04ac0d3e451711931cde
Denys Lins <denys.lins@hp.com>
2020-04-27 23:17:03 -0300

    Add debug switch

--- client/src/components/FireList/index.js
+++ client/src/components/FireList/index.js
@@ -1,27 +1,50 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import Switch from '@material-ui/core/Switch'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import { withStyles } from '@material-ui/core/styles'
+import { orange } from '@material-ui/core/colors'
 import './style.css'
 import Fire from '../Fire/index'
 
 const fireColorsPalette = [
   { r: 7, g: 7, b: 7 },
+  { r: 31, g: 7, b: 7 },
   { r: 47, g: 15, b: 7 },
+  { r: 71, g: 15, b: 7 },
   { r: 87, g: 23, b: 7 },
+  { r: 103, g: 31, b: 7 },
   { r: 119, g: 31, b: 7 },
+  { r: 143, g: 39, b: 7 },
   { r: 159, g: 47, b: 7 },
+  { r: 175, g: 63, b: 7 },
   { r: 191, g: 71, b: 7 },
+  { r: 199, g: 71, b: 7 },
   { r: 223, g: 79, b: 7 },
   { r: 223, g: 87, b: 7 },
+  { r: 223, g: 87, b: 7 },
   { r: 215, g: 95, b: 7 },
+  { r: 215, g: 95, b: 7 },
+  { r: 215, g: 103, b: 15 },
   { r: 207, g: 111, b: 15 },
+  { r: 207, g: 119, b: 15 },
   { r: 207, g: 127, b: 15 },
+  { r: 207, g: 135, b: 23 },
   { r: 199, g: 135, b: 23 },
+  { r: 199, g: 143, b: 23 },
   { r: 199, g: 151, b: 31 },
   { r: 191, g: 159, b: 31 },
+  { r: 191, g: 159, b: 31 },
   { r: 191, g: 167, b: 39 },
+  { r: 191, g: 167, b: 39 },
+  { r: 191, g: 175, b: 47 },
   { r: 183, g: 175, b: 47 },
+  { r: 183, g: 183, b: 47 },
   { r: 183, g: 183, b: 55 },
+  { r: 207, g: 207, b: 111 },
   { r: 223, g: 223, b: 159 },
+  { r: 239, g: 239, b: 199 },
   { r: 255, g: 255, b: 255 }
 ]
 
@@ -29,9 +52,11 @@ const FireList = (props) => {
   const [firePixelArray, setFirePixelArray] = useState([])
   const [fireWidth, setfireWidth] = useState(40)
   const [fireHeight, setfireHeight] = useState(20)
-  const [decay, setDecay] = useState(3)
+  const [vdecay, setVDecay] = useState(6)
+  const [hdecay, setHDecay] = useState(4)
   const [time, setTime] = useState(200)
   const [debug, setDebug] = useState(false)
+  const [checked, setChecked] = useState(false)
 
   const calculateFirePropagation = () => {
     const newArr = [...firePixelArray]
@@ -41,11 +66,13 @@ const FireList = (props) => {
         const belowPixelIndex = currentPixelIndex + fireWidth
         if (belowPixelIndex < fireHeight * fireWidth) {
           const belowPixelFireIntensity = firePixelArray[belowPixelIndex]
-          const randomDecay = Math.floor(Math.random() * decay)
-          const calculatedFireIntensity = belowPixelFireIntensity - randomDecay
+          const randomVDecay = Math.floor(Math.random() * vdecay)
+          const randomHDecay = Math.floor(Math.random() * hdecay)
+          const calculatedFireIntensity =
+            belowPixelFireIntensity - randomVDecay
           const newFireIntensity =
             calculatedFireIntensity > 0 ? calculatedFireIntensity : 0
-          newArr[currentPixelIndex - randomDecay] = newFireIntensity
+          newArr[currentPixelIndex - randomHDecay] = newFireIntensity
         }
       }
     }
@@ -53,12 +80,11 @@ const FireList = (props) => {
   }
 
   const createFireDataStructure = () => {
-    console.log(window.innerWidth, window.innerHeight)
     const numberOfPixels = fireHeight * fireWidth
     const newArr = new Array(numberOfPixels).fill(0)
     for (let column = 0; column < fireWidth; column++) {
       const pixelIndex = numberOfPixels - fireWidth + column
-      newArr[pixelIndex] = 18
+      newArr[pixelIndex] = fireColorsPalette.length - 1
     }
     setFirePixelArray(newArr)
   }
@@ -72,8 +98,28 @@ const FireList = (props) => {
     return () => clearInterval(interval)
   }, [calculateFirePropagation])
 
+  const CustomSwitch = withStyles({
+    switchBase: {
+      color: orange[100],
+      '&$checked': {
+        color: orange[700]
+      },
+      '&$checked + $track': {
+        backgroundColor: orange[700]
+      }
+    },
+    checked: {},
+    track: {}
+  })(Switch)
+
+  const toggleChecked = () => {
+    setDebug((prev) => !prev)
+    setChecked((prev) => !prev)
+  }
+
   return (
-    <div className="FireList">
+    <>
+      <span className="name">Denys Lins</span>
       <table cellPadding={0} cellSpacing={0}>
         <tbody>
           {new Array(fireHeight).fill(0).map((row, i) => (
@@ -112,7 +158,14 @@ const FireList = (props) => {
           ))}
         </tbody>
       </table>
-    </div>
+      <FormGroup className="debug">
+        <FormControlLabel
+          control={<CustomSwitch checked={checked} onChange={toggleChecked} />}
+          label="Debug"
+          labelPlacement="start"
+        />
+      </FormGroup>
+    </>
   )
 }
 
