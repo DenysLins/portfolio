dcf385975d2cacc3c6bb33c7b1041d9d9c9fad38
Denys Lins <denyslins@gmail.com>
2022-01-15 10:47:02 -0300

    Update google font

--- client-next/src/pages/_document.tsx
+++ client-next/src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-import React from 'react';
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
 
 export default class MyDocument extends Document {
   render() {
@@ -7,7 +7,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
