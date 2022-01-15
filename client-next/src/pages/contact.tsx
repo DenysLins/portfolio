ae6c403f6dc83b71c99db5f0c01686564bc80652
Denys Lins <denyslins@gmail.com>
2022-01-15 15:41:57 -0300

    Add contatc links

--- client-next/src/pages/contact.tsx
+++ client-next/src/pages/contact.tsx
@@ -1,15 +1,47 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import EmailIcon from "@mui/icons-material/Email";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-import style from "@/styles/about.module.scss";
+import style from "@/styles/contact.module.scss";
 
 const Contact = () => {
   const { t } = useTranslation("contact");
 
   return (
     <div className={style.container}>
-      <h1>{t("email")}</h1>
+      <div className={style.contact}>
+        <EmailIcon />
+        <a
+          target="_blank"
+          href={`mailto:denyslins@gmail.com?subject=${t("subject")}`}
+          rel="noopener noreferrer"
+        >
+          {t("email")}
+        </a>
+      </div>
+      <div className={style.contact}>
+        <TwitterIcon />
+        <a
+          target="_blank"
+          href="https://twitter.com/Denys_Lins"
+          rel="noopener noreferrer"
+        >
+          {t("twitter")}
+        </a>
+      </div>
+      <div className={style.contact}>
+        <LinkedInIcon />
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/denyslins"
+          rel="noopener noreferrer"
+        >
+          {t("linkedin")}
+        </a>
+      </div>
     </div>
   );
 };
