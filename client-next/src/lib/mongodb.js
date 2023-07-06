f67042e45f14d306012746132b6a7656a17cb4d7
Denys Lins <denyslins@gmail.com>
2023-07-06 20:41:12 -0300

    chore: fix mongoose error
    Fox mongoose error when running the build

--- client-next/src/lib/mongodb.js
+++ client-next/src/lib/mongodb.js
@@ -26,6 +26,8 @@ async function dbConnect() {
       bufferCommands: false,
     };
 
+    mongoose.set('strictQuery', false);
+
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
     });
