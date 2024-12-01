a3ab41d638a9234a4d787190eb416290f7f80451
Denys Lins <denyslins@gmail.com>
2024-12-01 15:27:02 -0300

    feat: 🎸 remove old client

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-
-import App from './components/App'
-import './index.css'
-import './i18n'
-
-ReactDOM.render(<App />, document.getElementById('root'))
